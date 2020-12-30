54d7f5f9baa29f7b7800a4a19bcd9fc7b72bd806
pashaRasta <zlodei2991@gmail.com>
2020-12-30 16:45:53 +0300

    add wow.js add animate.css

--- sever-security/js/main.js
+++ sever-security/js/main.js
@@ -20,6 +20,7 @@ $('a[href^="#"').on('click', function() {
   return false;
 });
 
+//Swiper slider
   var swiper = new Swiper('.swiper-container', {
     slidesPerView: 1,
     spaceBetween: 2,
@@ -33,6 +34,24 @@ $('a[href^="#"').on('click', function() {
       prevEl: '.swiper-button-prev',
     },
   });
+
+//wow.js scrolling
+var wow = new WOW(
+  {
+    boxClass:     'wow',      // класс, скрывающий элемент до момента отображения на экране (по умолчанию, wow)
+    animateClass: 'animated', // класс для анимации элемента (по умолчанию, animated)
+    offset:       0,          // расстояние в пикселях от нижнего края браузера до верхней границы элемента, необходимое для начала анимации (по умолчанию, 0)
+    mobile:       true,       // включение/отключение WOW.js на мобильных устройствах (по умолчанию, включено)
+    live:         true,       // поддержка асинхронно загруженных элементов (по умолчанию, включена)
+    callback:     function(box) {
+      // функция срабатывает каждый раз при старте анимации
+      // аргумент box — элемент, для которого была запущена анимация
+    },
+    scrollContainer: null // селектор прокручивающегося контейнера (опционально, по умолчанию, window)
+  }
+);
+wow.init();
+
 
 //залипание меню
   // $(document).ready(function() {
