aaff80ad131b399d00d76442e2d3f17c3056a453
pashaRasta <zlodei2991@gmail.com>
2021-05-08 22:16:33 +0300

    new project

---
+++ gazobeton/js/main.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper('.swiper-container', {
+      grabCursor: true,
+      cubeEffect: {
+        shadow: true,
+        slideShadows: true,
+        shadowOffset: 10,
+        shadowScale: 0.4,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+      },
+ });
